0fa3394ec039a11f4ee280aeef49f63eaf81fe6b
Kaushik Kashyap <138221951+ClownbP@users.noreply.github.com>
2023-08-26 20:05:11 +0530

    Create a form using Swing on Netbeans

---
+++ GradeCalc/src/main/java/com/mycompany/gradecalc/GradeCalc.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradecalc;
+
+
+public class  GradeCalc {
+
+    public static void main(String[] args) {
+       
+    }
+}
--- GradeCalcMain.java
+++
@@ -1,8 +0,0 @@
-
-public class GradeCalcMain {
-    public static void main(String[] args) {
-        new Myframe();
-        
-    }
-
-}
